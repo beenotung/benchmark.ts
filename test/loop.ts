d5e322a077c0968271782c665c91d544d6c94545
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-03-04 13:36:50 +0800

    update test/loop.ts to use sync version of estimate function

--- test/loop.ts
+++ test/loop.ts
@@ -1,50 +1,49 @@
-import { DoneFn, estimate, EstimateOptions, report } from '..';
+import { EstimateOptions, report } from '../utils'
+import { estimateSync } from '../sync'
 
 // config
 let options: EstimateOptions = {
   nameFn: (fn) => fn.name.replace('test', ''),
   maxErrorPercentage: 5,
   minSample: 5,
-};
-let n = 77001 * 3;
-let xs = new Array(n).fill(0).map((_, i) => ({ id: i, name: 'i' + i }));
+}
+let n = 77001 * 3
+let xs = new Array(n).fill(0).map((_, i) => ({ id: i, name: 'i' + i }))
 
 // test functions
-function forOf(done: DoneFn) {
+function forOf() {
   for (let x of xs) {
-    x.id + x.name;
+    x.id + x.name
   }
-  done();
 }
-function forEach(done: DoneFn) {
-  xs.forEach((x) => x.id + x.name);
-  done();
+
+function forEach() {
+  xs.forEach((x) => x.id + x.name)
 }
-function forI(done: DoneFn) {
+
+function forI() {
   for (let i = 0; i < xs.length; i++) {
-    let x = xs[i];
-    x.id + x.name;
+    let x = xs[i]
+    x.id + x.name
   }
-  done();
 }
 
 // warm up
 for (let i = 0; i < 5; i++) {
-  forOf(console.log);
-  forEach(console.log);
-  forI(console.log);
+  forOf()
+  forEach()
+  forI()
 }
 
 // benchmark
-estimate(forOf, options, (forOfMs) => {
-  report(forOf, forOfMs);
-  estimate(forEach, options, (forEachMs) => {
-    report(forOf, forOfMs);
-    report(forEach, forEachMs);
-    estimate(forI, options, (forIMs) => {
-      report(forOf, forOfMs);
-      report(forEach, forEachMs);
-      report(forI, forIMs);
-    });
-  });
-});
+let forOfMs = estimateSync(forOf, options)
+report(forOf, forOfMs)
+
+let forEachMs = estimateSync(forEach, options)
+report(forOf, forOfMs)
+report(forEach, forEachMs)
+
+let forIMs = estimateSync(forI, options)
+report(forOf, forOfMs)
+report(forEach, forEachMs)
+report(forI, forIMs)
